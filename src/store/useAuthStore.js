d8400413d9f55bb5f0b9353a0e4669485ca66318
Ziyodullo Rakhmonberdiev (Ali) <ziyodillo808@gmail.com>
2025-12-13 23:39:51 +0300

    project started & signup page created

---
+++ src/store/useAuthStore.js
@@ -0,0 +1,53 @@
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios.js";
+import toast from "react-hot-toast";
+
+export const useAuthStore = create((set) => ({
+  authUser: null,
+
+  // for loaders
+  isCheckingAuth: true,
+  isSigningUp: false,
+  isLoggingIn: false,
+  isUpdatingProfile: false,
+
+  // check the auth and set states
+  checkAuth: async () => {
+    try {
+      const res = await axiosInstance.get("/auth/check");
+      set({ authUser: res.data });
+    } catch (error) {
+      console.log("Error in checkAuth: ", error.message);
+      set({ authUser: null });
+    } finally {
+      set({ isCheckingAuth: false });
+    }
+  },
+
+  signup: async (data) => {
+    set({ isSigningUp: true });
+    try {
+      const res = await axiosInstance.post("/auth/signup", data);
+      toast.success("Account created successfully");
+      set({ authUser: res.data });
+    } catch (error) {
+      console.log("Error in signin: ", error.message);
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isSigningUp: false });
+    }
+  },
+
+  logout: async () => {
+    try {
+      await axiosInstance.post("/auth/logout");
+      set({ authUser: null });
+      toast.success("Logged out successfully");
+    } catch (error) {
+      console.log(
+        "Something went wrong when logout",
+        error.response.data.message
+      );
+    }
+  },
+}));
