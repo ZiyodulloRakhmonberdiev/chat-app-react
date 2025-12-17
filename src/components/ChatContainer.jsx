815fa078299c7866bb1612547fe2291162d96d9f
Ziyodullo Rakhmonberdiev (Ali) <ziyodillo808@gmail.com>
2025-12-17 22:08:56 +0300

    message input component created

--- src/components/ChatContainer.jsx
+++ src/components/ChatContainer.jsx
@@ -1,5 +1,7 @@
 import { useEffect } from "react";
 import { useChatStore } from "../store/useChatStore";
+import ChatHeader from "./ChatHeader";
+import MessageInput from "./MessageInput";
 
 const ChatContainer = () => {
   const { messages, getMessages, isMessagesLoading, selectedUser } =
@@ -9,8 +11,23 @@ const ChatContainer = () => {
     getMessages(selectedUser._id);
   }, [selectedUser._id, getMessages]);
 
-  if (isMessagesLoading) return <div>Loading...</div>;
-  return <div>ChatContainer</div>;
+  if (isMessagesLoading) {
+    return (
+      <div className="flex flex-1 overflow-auto flex-col">
+        <ChatHeader />
+        <div>Messages...</div>
+        <MessageInput />
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col flex-1 overflow-auto">
+      <ChatHeader />
+      <div>Messages...</div>
+      <MessageInput />
+    </div>
+  );
 };
 
 export default ChatContainer;
--- src/store/useChatStore.js
+++ src/store/useChatStore.js
@@ -2,7 +2,7 @@ import { create } from "zustand";
 import toast from "react-hot-toast";
 import { axiosInstance } from "../lib/axios";
 
-export const useChatStore = create((set) => ({
+export const useChatStore = create((set, get) => ({
   messages: [],
   users: [],
   selectedUser: null,
@@ -12,7 +12,7 @@ export const useChatStore = create((set) => ({
   getUsers: async () => {
     set({ isUsersLoading: true });
     try {
-      const res = await axiosInstance.get("/message/users");
+      const res = await axiosInstance.get("/messages/users");
       set({ users: res.data });
     } catch (error) {
       toast.error(error.response.data.message);
@@ -24,7 +24,7 @@ export const useChatStore = create((set) => ({
   getMessages: async (userId) => {
     set({ isMessagesLoading: true });
     try {
-      const res = await axiosInstance.get(`/message/${userId}`);
+      const res = await axiosInstance.get(`/messages/${userId}`);
       set({ messageg: res.data });
     } catch (error) {
       toast.error();
@@ -33,6 +33,21 @@ export const useChatStore = create((set) => ({
     }
   },
 
+  sendMessage: async (messageData) => {
+    const { selectedUser, messages } = get();
+    try {
+      const res = await axiosInstance.post(
+        `/messages/send/${selectedUser._id}`,
+        messageData
+      );
+      console.log(res.data);
+      
+      set({ messages: [...messages, res.data] });
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  },
+
   // optimize this one later
-  setSelectedUser: async (selectedUser) => ({selectedUser}),
+  setSelectedUser: async (selectedUser) => set({ selectedUser }),
 }));
